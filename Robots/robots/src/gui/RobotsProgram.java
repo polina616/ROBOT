d042afbe2119b85f504f101d9744ce3057df67e6
varvara <sofam2135@gmail.com>
2026-02-20 22:17:29 +0500

    добавлен maven

--- Robots/robots/src/gui/RobotsProgram.java
+++ Robots/robots/src/gui/RobotsProgram.java
@@ -8,28 +8,32 @@ import org.omegat.swing.extra.ExtraLocales;
 public class RobotsProgram
 {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.of("ru", "RU"));
+        Locale.setDefault(new Locale("ru", "RU"));
+
+
+//        try {
+//            ExtraLocales.initialize();
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+
+
+        UIManager.put("OptionPane.yesButtonText", "ДА");
+        UIManager.put("OptionPane.noButtonText", "НЕТ");
+        UIManager.put("OptionPane.okButtonText", "ОКЕЙ");
+        UIManager.put("OptionPane.cancelButtonText", "ОТМЕНА");
+
         try {
-            ExtraLocales.initialize();
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //UIManager.put("OptionPane.yesButtonText", "ДА");
-        //UIManager.put("OptionPane.noButtonText", "НЕТ");
-        //UIManager.put("OptionPane.okButtonText", "ОКЕЙ");
-        //UIManager.put("OptionPane.cancelButtonText", "ОТМЕНА");
-      try {
-        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-//        UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-//        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      SwingUtilities.invokeLater(() -> {
-        MainApplicationFrame frame = new MainApplicationFrame();
-        frame.pack();
-        frame.setVisible(true);
-        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-      });
-    }}
+
+        SwingUtilities.invokeLater(() -> {
+            MainApplicationFrame frame = new MainApplicationFrame();
+            frame.pack();
+            frame.setVisible(true);
+            frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+        });
+    }
+    }
